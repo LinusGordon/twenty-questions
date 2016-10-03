31e56be570b509737ad190067f9c84ecffb1c7e2
gregorybchris <gregorybchris@gmail.com>
2016-10-03 15:53:16 -0400

    Added new styling

--- playgame.js
+++ playgame.js
@@ -1,12 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
-
-	function play() {
-		document.getElementById("gameboard").innerHTML = "????????????????????"
-	}
-	document.getElementById('play').onclick = play;
-
-	function clear() {
-		document.getElementById("gameboard").innerHTML = ""
-	}
-	document.getElementById('clear').onclick = clear;
+	document.getElementById('play-button').onclick = function() {
+		var playButtonText = document.getElementById('play-button-text');
+		var gameboardText = document.getElementById("gameboard-text")
+		
+		if (playButtonText.innerHTML == "Play") {
+			playButtonText.innerHTML = "Replay";
+			gameboardText.className = "";
+		}
+		else {
+			playButtonText.innerHTML = "Play";
+			gameboardText.className = "hidden";
+		}
+	};
 });
